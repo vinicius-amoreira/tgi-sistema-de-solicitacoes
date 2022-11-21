03cf5d0e5f6ad6dd2f8364f1cff19123e9427b19
Daniel de Souza Cordeiro <dcordeiro962@gmail.com>
2022-11-21 16:29:10 -0300

    teste deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // postLogin: 'https://material-request-production.up.railway.app/users/auth/sessions',
-  // apiUrl: 'https://material-request-production.up.railway.app'
-  postLogin: 'http://localhost:80/users/auth/sessions',
-  apiUrl: 'http://localhost:80'
+  postLogin: 'https://material-request-production.up.railway.app/users/auth/sessions',
+  apiUrl: 'https://material-request-production.up.railway.app'
+  // postLogin: 'http://localhost:80/users/auth/sessions',
+  // apiUrl: 'http://localhost:80'
 };
